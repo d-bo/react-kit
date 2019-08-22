0cacb75d6a05bd610a99f2f8a0cdbb87c43b5d3a
d-bo <vathsven@pm.me>
2019-08-23 00:26:51 +0500

    react-router lazy loading components

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,18 +1,51 @@
 import Navbar from "../app-navbar";
 import Footer from "../app-footer";
-import { connect } from "react-redux";
-import * as firebase from "firebase/app";
-import React, { Component } from "react";
-import { firebaseLogOut } from "../../redux/actions";
+import firebase from "firebase/app";
+import React, { lazy, Suspense } from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import Reset from "../app-auth/reset";
-import SignIn from "../app-auth/signin";
-import Profile from "../app-profile";
-import Person from "../app-person";
-import Register from "../app-auth/register";
-import Home from "../app-home";
-import NotFound404 from "../app-404";
 import { FirebaseUserContext } from "../../contexts/FirebaseUserContext";
+
+const ResetComponent = (
+  lazy(() => (
+    import("../app-auth/reset")
+  ))
+);
+
+const SignInComponent = (
+  lazy(() => (
+    import("../app-auth/signin")
+  ))
+);
+
+const ProfileComponent = (
+  lazy(() => (
+    import("../app-profile")
+  ))
+);
+
+const HomeComponent = (
+  lazy(() => (
+    import("../app-home")
+  ))
+);
+
+const RegisterComponent = (
+  lazy(() => (
+    import("../app-auth/register")
+  ))
+);
+
+const PersonComponent = (
+  lazy(() => (
+    import("../app-person")
+  ))
+);
+
+const NotFound404Component = (
+  lazy(() => (
+    import("../app-404")
+  ))
+);
 
 interface IAppProps {
   firebaseUser: firebase.User | null;
@@ -60,15 +93,31 @@ class App extends React.Component<IAppProps, IAppState> {
         }}>
           <Navbar {...this.props} />
             <Router history={history}>
-              <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/profile" component={Profile} />
-                <Route path="/person/:id" component={Person} />
-                <Route path="/auth/signin" component={SignIn} />
-                <Route path="/auth/register" component={Register} />
-                <Route path="/auth/reset" component={Reset} />
-                <Route component={NotFound404} />
-              </Switch>
+              <Suspense fallback="Loading ...">
+                <Switch>
+                  <Route path="/" exact>
+                    <HomeComponent/>
+                  </Route>
+                  <Route path="/profile">
+                    <ProfileComponent/>
+                  </Route>
+                  <Route path="/person/:id">
+                    <PersonComponent/>
+                  </Route>
+                  <Route path="/auth/signin">
+                    <SignInComponent/>
+                  </Route>
+                  <Route path="/auth/register">
+                    <RegisterComponent/>
+                  </Route>
+                  <Route path="/auth/reset">
+                    <ResetComponent/>
+                  </Route>
+                  <Route>
+                    <NotFound404Component/>
+                  </Route>
+                </Switch>
+              </Suspense>
             </Router>
           <Footer />
         </FirebaseUserContext.Provider>
